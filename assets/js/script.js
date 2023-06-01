ef8e464a2ea513fe2aaf004b2bcf24085874c0bc
Mostafa-hussaini <mostafaislami97@gmail.com>
2023-06-01 09:35:42 +0430

    page navigation functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,27 @@
 "use strict";
 
-
 /**
  * Initiate glightbox
  */
 const glightbox = GLightbox({
   selector: ".glightbox",
 });
- 
+
 /**
  * Date & time
  */
 
- 
+
+/**
+ * Page Navigation
+ */
+
+document.querySelectorAll(".nav-link").forEach((el) => {
+  el.addEventListener("click", (e) => {
+    e.preventDefault();
+    const id = el.getAttribute("href");
+    document.querySelector(id).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
