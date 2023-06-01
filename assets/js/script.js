eef636b6a63f23b9dc67130f466286cbcb22460b
Mostafa-hussaini <mostafaislami97@gmail.com>
2023-06-01 17:22:52 +0430

    scrollTop and section reveal added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,15 @@
 "use strict";
 
 /**
+ * All Selectors
+ */
+const scrollToTop = document.querySelector(".scroll-top");
+const header = document.querySelector("#header");
+const aboutSection = document.querySelector("#about");
+const navLinks = document.querySelectorAll(".nav-link");
+const sections = document.querySelectorAll("section");
+
+/**
  * Initiate glightbox
  */
 const glightbox = GLightbox({
@@ -11,6 +20,30 @@ const glightbox = GLightbox({
  * Date & time
  */
 
+/**
+ *  Scroll to Top
+ */
+
+scrollToTop.addEventListener("click", () => {
+  header.scrollIntoView({
+    behavior: "smooth",
+  });
+});
+
+// Button visibility
+
+const scrollTop = (entries) => {
+  const [entry] = entries;
+  if (!entry.isIntersecting) scrollToTop.classList.add("active");
+  else scrollToTop.classList.remove("active");
+};
+
+const obsOptions = {
+  root: null,
+  threshold: 0.6,
+};
+const aboutSectionObserver = new IntersectionObserver(scrollTop, obsOptions);
+aboutSectionObserver.observe(aboutSection);
 
 /**
  * Page Navigation
@@ -24,4 +57,49 @@ document.querySelector(".navbar-nav").addEventListener("click", (e) => {
       behavior: "smooth",
     });
   }
+});
+
+// Nav Link Activation
+const navLinkActive = (entries) => {
+  const [entry] = entries;
+  const id = entry.target.id;
+  if (entry.isIntersecting) {
+    navLinks.forEach((link) => {
+      link.getAttribute("href") == `#${id}`
+        ? link.classList.add("active")
+        : link.classList.remove("active");
+    });
+  }
+};
+
+const navLinkOptions = {
+  root: null,
+  threshold: 0.7,
+};
+
+const sectionObserver = new IntersectionObserver(navLinkActive, navLinkOptions);
+sections.forEach((section) => {
+  sectionObserver.observe(section);
+});
+
+/**
+ * Reveal Sections
+ */
+
+const revealSection = (entries, observer) => {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("hidden");
+  observer.unobserve(entry.target);
+};
+
+const revealOptions = {
+  root: null,
+  threshold: 0.15,
+};
+
+const secObserver = new IntersectionObserver(revealSection, revealOptions);
+sections.forEach((section) => {
+  secObserver.observe(section);
+  section.classList.add("hidden");
 });
